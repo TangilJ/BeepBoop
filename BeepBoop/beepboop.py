ed21ebaf001608003d568b2f76372acacbe74bda
TheBlocks <21197843+TheBlocks@users.noreply.github.com>
2018-12-08 20:48:13 +0100

    Create step_handler.py, add StepHandler to BeepBoop

--- BeepBoop/beepboop.py
+++ BeepBoop/beepboop.py
@@ -1,13 +1,15 @@
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 from BeepBoop.utils.quick_chat_handler import QuickChatHandler
+from BeepBoop.steps.step_handler import StepHandler
 
 
 class BeepBoop(BaseAgent):
     def initialize_agent(self) -> None:
         self.quick_chat_handler: QuickChatHandler = QuickChatHandler(self)
+        self.step_handler: StepHandler = StepHandler(self)
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         self.quick_chat_handler.handle_quick_chats(packet)
 
-        return SimpleControllerState()
+        return self.step_handler.get_output(packet)
---
+++ BeepBoop/steps/step_handler.py
@@ -0,0 +1,40 @@
+from rlbot.agents.base_agent import BaseAgent, GameTickPacket, SimpleControllerState
+from BeepBoop.steps.base_step import BaseStep
+from BeepBoop.steps.kickoff_step import KickoffStep
+from BeepBoop.steps.simple_dribble_step import SimpleDribbleStep
+from BeepBoop.steps.simple_move_step import SimpleMoveStep
+from BeepBoop.bot_math.Vector3 import Vector3
+from typing import Optional
+
+
+class StepHandler:
+    def __init__(self, agent: BaseAgent):
+        self.agent: BaseAgent = agent
+        self.current_step: BaseStep = KickoffStep(self.agent)
+
+    def choose_step(self, packet: GameTickPacket) -> BaseStep:
+        ball = Vector3(packet.game_ball.physics.location)
+        bot = Vector3(packet.game_cars[self.agent.index].physics.location)
+
+        if ball.x == 0 and ball.y == 0:
+            return KickoffStep(self.agent)
+        # Go to bot's own goal if the ball is in between the bot and the bot's own goal
+        elif (bot.y < ball.y) if self.agent.team else (bot.y > ball.y):
+            return SimpleMoveStep(self.agent, Vector3(0, 5000 * 1 if self.agent.team else -1, 0))
+        else:
+            return SimpleDribbleStep(self.agent)
+
+    def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
+        out: Optional[SimpleControllerState] = self.current_step.get_output(packet)
+
+        if not self.current_step.cancellable and out is not None:
+            return out
+
+        self.current_step = self.choose_step(packet)
+        out = self.current_step.get_output(packet)
+
+        if out is not None:
+            return out
+        else:
+            self.agent.logger.warning(f"Agent {self.agent.name} is returning an empty controller on the first frame of the step")
+            return SimpleControllerState()
