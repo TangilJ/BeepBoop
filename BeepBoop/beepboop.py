dbdc5ab39a0ede1ea01d2ee718100bd01d6ce70e
TheBlocks <21197843+TheBlocks@users.noreply.github.com>
2018-12-08 11:03:03 +0100

    Add quick chat handling to BeepBoop

--- BeepBoop/beepboop.py
+++ BeepBoop/beepboop.py
@@ -1,7 +1,13 @@
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
+from BeepBoop.utils.quick_chat_handler import QuickChatHandler
 
 
 class BeepBoop(BaseAgent):
+    def initialize_agent(self) -> None:
+        self.quick_chat_handler: QuickChatHandler = QuickChatHandler(self)
+
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
+        self.quick_chat_handler.handle_quick_chats(packet)
+
         return SimpleControllerState()
