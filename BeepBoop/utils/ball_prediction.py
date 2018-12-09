185a47a7289e68b800ae1d91236158e783dfdc13
TheBlocks <21197843+TheBlocks@users.noreply.github.com>
2018-12-09 11:52:44 +0100

    Fix ZeroDivisionError in ball_prediction.py

--- BeepBoop/utils/ball_prediction.py
+++ BeepBoop/utils/ball_prediction.py
@@ -8,10 +8,21 @@ def get_ground_bounces(path: BallPrediction) -> List[Tuple[Vector3, float]]:
 
     for i in range(1, path.num_slices):
         prev_ang_vel: Vector3 = Vector3(path.slices[i - 1].physics.angular_velocity)
-        prev_ang_vel_normalised: Vector3 = prev_ang_vel.normalised()
+
+        # Make sure it's never (0, 0, 0) to avoid ZeroDivisionError
+        if prev_ang_vel == Vector3(0, 0, 0):
+            prev_ang_vel_normalised: Vector3 = Vector3(1, 1, 1)
+        else:
+            prev_ang_vel_normalised: Vector3 = prev_ang_vel.normalised()
+
         current_slice = path.slices[i]
         current_ang_vel: Vector3 = Vector3(current_slice.physics.angular_velocity)
-        current_ang_vel_normalised: Vector3 = current_ang_vel.normalised()
+
+        # Make sure it's never (0, 0, 0) to avoid ZeroDivisionError
+        if current_ang_vel == Vector3(0, 0, 0):
+            current_ang_vel_normalised: Vector3 = Vector3(1, 1, 1)
+        else:
+            current_ang_vel_normalised: Vector3 = current_ang_vel.normalised()
 
         # Ball's angular velocity does not change in air; it only changes when bouncing.
         # Therefore, if the ball changes angular velocity between frames and the height is low, a bounce occurred.
