5d905cb3c2a351273ba597abd4d1c151edd6d73a
Redox <exploreblocks@gmail.com>
2018-12-11 17:50:47 +0100

    Add gosling_steering function to steering.py

--- BeepBoop/utils/steering.py
+++ BeepBoop/utils/steering.py
@@ -1,8 +1,10 @@
 from BeepBoop.bot_math.Vector3 import Vector3
+from BeepBoop.utils import calculations
 import math
 
 
 def simple_aim(position: Vector3, yaw: float, target: Vector3) -> float:
+    """Bang bang controller for steering."""
     pos_to_target: Vector3 = target - position
     facing: Vector3 = Vector3(math.cos(yaw), math.sin(yaw), 0)
     self_right: Vector3 = Vector3.cross_product(facing, Vector3(0, 0, 1))
@@ -11,3 +13,13 @@ def simple_aim(position: Vector3, yaw: float, target: Vector3) -> float:
         return 1.0
     else:
         return -1.0
+
+
+def gosling_steering(position: Vector3, yaw: float, target: Vector3) -> float:
+    """Simple steering used in Gosling"""
+    # Absolutely shamelessly stealing this from Gosling (sorry Goose)
+    # https://github.com/ddthj/Gosling/blob/master/Episode%205%20Code/Util.py#L128
+    angle_to_ball: float = calculations.angle_to_target(position, yaw, target)
+    steer_value: float = ((10 * angle_to_ball + math.copysign(1, angle_to_ball)) ** 3) / 20
+
+    return steer_value
