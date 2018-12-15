3513c8cd4f222ea45ee3b7ad54004bda670d4978
TheBlocks <21197843+TheBlocks@users.noreply.github.com>
2018-12-15 12:46:49 +0100

    Change kickoffs to Jeroen's kick off trainer's kickoffs

--- BeepBoop/steps/kickoff_step.py
+++ BeepBoop/steps/kickoff_step.py
@@ -1,13 +1,13 @@
-from rlbot.utils.structures.game_data_struct import GameTickPacket
+from rlbot.utils.structures.game_data_struct import GameTickPacket, BoostPad
 from rlbot.agents.base_agent import SimpleControllerState, BaseAgent
 from BeepBoop.steps.base_step import BaseStep
 from BeepBoop.bot_math.Vector3 import Vector3
 from typing import List, Optional, Union
-from RLUtilities.Maneuvers import AirDodge, Drive, DoNothing
+from RLUtilities.Maneuvers import AirDodge, Drive
 from RLUtilities.GameInfo import GameInfo
 from RLUtilities.LinearAlgebra import vec3, normalize
 
-KICKOFF_ACTION = Union[AirDodge, Drive, DoNothing]
+KICKOFF_ACTION = Union[AirDodge, Drive]
 
 
 class KickoffStep(BaseStep):
@@ -18,38 +18,58 @@ class KickoffStep(BaseStep):
         self.kickoff_steps: Optional[List[KICKOFF_ACTION]] = None
         self.game_info: GameInfo = GameInfo(agent.index, agent.team)
 
-    def prepare_kickoff(self, packet: GameTickPacket) -> None:
-        bot = Vector3(packet.game_cars[self.agent.index].physics.location)
+    def get_closest_small_pad(self, bot_pos: Vector3) -> BoostPad:
+        closest_pad: BoostPad = None
+        dist_to_closest_pad: float = float("inf")
 
-        # DoNothing is put before AirDodge because we don't want the bot to boost when it's dodging,
-        # and so we reset the controller first
-        if abs(bot.x) < 250:
-            # Centre kickoff
+        for boost_pad in self.agent.get_field_info().boost_pads:
+            current_pad: BoostPad = boost_pad
+            current_pos: Vector3 = Vector3(current_pad.location)
+            dist_to_current_pad = Vector3.distance(bot_pos, current_pos)
+
+            if not current_pad.is_full_boost and dist_to_current_pad < dist_to_closest_pad:
+                dist_to_closest_pad = dist_to_current_pad
+                closest_pad = current_pad
+
+        return closest_pad
+
+    def prepare_kickoff(self, packet: GameTickPacket) -> None:
+        bot_pos = Vector3(packet.game_cars[self.agent.index].physics.location)
+
+        # Centre kickoff
+        if abs(bot_pos.x) < 250:
+            pad = self.get_closest_small_pad(bot_pos).location
+            first_target: vec3 = vec3(pad.x, pad.y, pad.z) - vec3(0, 250, 0) * (1 if self.agent.team else -1)
+            second_target: vec3 = vec3(0, 850, 0) * (1 if self.agent.team else -1)
+
             self.kickoff_steps = [
-                Drive(self.game_info.my_car, vec3(0, 3500 * (1 if self.agent.team else -1), 0), 2400),
-                DoNothing(),
-                AirDodge(self.game_info.my_car, 0.1, vec3(0, 0, 0)),
-                Drive(self.game_info.my_car, vec3(0, 300 * (1 if self.agent.team else -1), 0), 2400),
-                DoNothing(),
-                AirDodge(self.game_info.my_car, 0.1, vec3(0, 0, 0)),
+                Drive(self.game_info.my_car, first_target, 2400),
+                AirDodge(self.game_info.my_car, 0.075, vec3(0, 0, 0)),
+                Drive(self.game_info.my_car, second_target, 2400),
+                AirDodge(self.game_info.my_car, 0.075, vec3(0, 0, 0))
             ]
-        elif abs(bot.x) < 1000:
-            # Off-centre kickoff
+        # Off-centre kickoff
+        elif abs(bot_pos.x) < 1000:
+            target: vec3 = normalize(self.game_info.my_car.pos) * 500
+
             self.kickoff_steps = [
-                Drive(self.game_info.my_car, vec3(0, 2700 * (1 if self.agent.team else -1), 0), 2400),
-                Drive(self.game_info.my_car, vec3(0, 1000 * (1 if self.agent.team else -1), 0), 2400),
-                DoNothing(),
-                AirDodge(self.game_info.my_car, 0.1, vec3(0, 0, 0)),
-                Drive(self.game_info.my_car, vec3(0, 300 * (1 if self.agent.team else -1), 0), 2400),
-                DoNothing(),
-                AirDodge(self.game_info.my_car, 0.1, vec3(0, 0, 0)),
+                Drive(self.game_info.my_car, vec3(self.game_info.my_car.pos[0], 3477 * (1 if self.agent.team else -1), 0), 2400),
+                AirDodge(self.game_info.my_car, 0.075, vec3(0, 0, 0)),
+                Drive(self.game_info.my_car, target, 2400),
+                AirDodge(self.game_info.my_car, 0.075, vec3(0, 0, 0))
             ]
+        # Diagonal kickoff
         else:
-            spawn_to_ball_norm: vec3 = normalize(self.game_info.my_car.pos)
-            # Diagonal kickoff
+            pad = self.get_closest_small_pad(bot_pos).location
+            car_to_pad: vec3 = vec3(pad.x, pad.y, pad.z) - self.game_info.my_car.pos
+            first_target: vec3 = self.game_info.my_car.pos + 1.425 * car_to_pad
+            second_target: vec3 = vec3(0, 150, 0) * (1 if self.agent.team else -1)
+            third_target: vec3 = normalize(self.game_info.my_car.pos) * 850
+
             self.kickoff_steps = [
-                Drive(self.game_info.my_car, vec3(0, 0, 0) + spawn_to_ball_norm * 200, 2400),
-                DoNothing(),
+                Drive(self.game_info.my_car, first_target, 2300),
+                AirDodge(self.game_info.my_car, 0.035, second_target),
+                Drive(self.game_info.my_car, third_target, 2400),
                 AirDodge(self.game_info.my_car, 0.1, vec3(0, 0, 0))
             ]
 
@@ -66,22 +86,25 @@ class KickoffStep(BaseStep):
         if len(self.kickoff_steps) > 0:
             step: KICKOFF_ACTION = self.kickoff_steps[0]
             while step.finished:
-                if isinstance(step, DoNothing):
-                    self.kickoff_steps.pop(0)
-                    step = self.kickoff_steps[0]
-                    return step.controls
-
                 if len(self.kickoff_steps) == 0:
                     self.cancellable = True
                     self.agent.logger.warning(f"Agent {self.agent.name}: Uh oh. Ran out of steps in kickoff.")
                     return None
+
                 self.kickoff_steps.pop(0)
+
                 # TODO: Fix this spaghetti-ass duplicate code
                 if len(self.kickoff_steps) == 0:
                     self.cancellable = True
                     self.agent.logger.warning(f"Agent {self.agent.name}: Uh oh. Ran out of steps in kickoff.")
                     return None
+
                 step = self.kickoff_steps[0]
+
+            if isinstance(step, Drive) and not self.game_info.my_car.on_ground:
+                # Do not do anything until the car gets on the ground
+                return SimpleControllerState()
+
             step.step(1 / 60)
             return step.controls
         else:
